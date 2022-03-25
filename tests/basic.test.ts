4b39e90d7430dbf76c8944b28cfd05eef3e32745
linux_china <libing.chen@gmail.com>
2022-03-25 12:40:46 -0700

    basic.test.ts

---
+++ tests/basic.test.ts
@@ -0,0 +1,12 @@
+import {describe, expect, it, test} from 'vitest'
+
+test('demo', () => {
+    expect(Math.sqrt(2)).toBe(Math.SQRT2)
+})
+
+describe('suite', () => {
+    it('demo2', () => {
+        console.log("demo2")
+    })
+})
+
