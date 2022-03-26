7ef446c4d8c5d457203d3fcb644aaed248208f39
linux_china <libing.chen@gmail.com>
2022-03-25 18:03:31 -0700

    Vitest

---
+++ src/main/kotlin/com/github/linuxchina/jetbrains/plugins/vitest/Vitest.kt
@@ -0,0 +1,8 @@
+package com.github.linuxchina.jetbrains.plugins.vitest
+
+import com.intellij.icons.AllIcons
+import com.intellij.openapi.util.IconLoader
+
+val vitestIcon = IconLoader.getIcon("/vitest-16.png", VitestRunnerMarkerProvider::class.java)
+val runIcon = IconLoader.getIcon("/runConfigurations/testState/run.svg", AllIcons::class.java)
+val runRunIcon = IconLoader.getIcon("/runConfigurations/testState/run_run.svg", AllIcons::class.java)
