16a092522d906b8bac60d0551f3cf45e5a0101ac
linux_china <libing.chen@gmail.com>
2022-03-25 17:44:50 -0700

    Add runRunIcon

--- src/main/kotlin/com/github/linuxchina/jetbrains/plugins/vitest/VitestBaseRunLineMarkerProvider.kt
+++ src/main/kotlin/com/github/linuxchina/jetbrains/plugins/vitest/VitestBaseRunLineMarkerProvider.kt
@@ -33,6 +33,7 @@ open class VitestBaseRunLineMarkerProvider : RunLineMarkerProvider() {
         val vitestTestMethodNames = listOf("test", "it", "describe")
         val vitestIcon = IconLoader.getIcon("/vitest-16.png", VitestRunnerMarkerProvider::class.java)
         val runIcon = IconLoader.getIcon("/runConfigurations/testState/run.svg", AllIcons::class.java)
+        val runRunIcon = IconLoader.getIcon("/runConfigurations/testState/run_run.svg", AllIcons::class.java)
     }
 
     fun isVitestTestMethod(jsCallExpression: JSCallExpression): Boolean {
