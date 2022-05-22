ce261ba2999c67727e93b6e3351da87e8a4dad03
linux_china <libing.chen@gmail.com>
2022-05-22 01:54:24 -0700

    feat: refresh tree and file open support

--- src/main/kotlin/com/github/linuxchina/jetbrains/plugins/vitest/ui/VitestToolWindowFactory.kt
+++ src/main/kotlin/com/github/linuxchina/jetbrains/plugins/vitest/ui/VitestToolWindowFactory.kt
@@ -1,33 +1,42 @@
 package com.github.linuxchina.jetbrains.plugins.vitest.ui
 
 import com.github.linuxchina.jetbrains.plugins.vitest.*
+import com.intellij.openapi.application.ApplicationManager
+import com.intellij.openapi.fileEditor.FileEditorManager
+import com.intellij.openapi.fileEditor.OpenFileDescriptor
 import com.intellij.openapi.project.DumbAware
 import com.intellij.openapi.project.Project
 import com.intellij.openapi.project.guessProjectDir
 import com.intellij.openapi.ui.SimpleToolWindowPanel
 import com.intellij.openapi.wm.ToolWindow
 import com.intellij.openapi.wm.ToolWindowFactory
+import com.intellij.psi.PsiManager
 import com.intellij.ui.components.JBScrollPane
 import com.intellij.ui.content.ContentFactory
 import com.intellij.ui.treeStructure.Tree
 import java.awt.Component
+import java.awt.event.MouseAdapter
+import java.awt.event.MouseEvent
 import javax.swing.JComponent
 import javax.swing.JTree
 import javax.swing.tree.DefaultMutableTreeNode
 import javax.swing.tree.DefaultTreeCellRenderer
+import javax.swing.tree.DefaultTreeModel
 
 
 class VitestToolWindowFactory : ToolWindowFactory, DumbAware {
     override fun createToolWindowContent(project: Project, toolWindow: ToolWindow) {
-        val jBangToolWindowPanel = JBangToolWindowPanel(project)
+        val vitestToolWindowPanel = VitestToolWindowPanel(project)
         val contentFactory = ContentFactory.SERVICE.getInstance()
-        val content = contentFactory.createContent(jBangToolWindowPanel, "", false)
+        val content = contentFactory.createContent(vitestToolWindowPanel, "", false)
         toolWindow.contentManager.addContent(content)
     }
 }
 
-class JBangToolWindowPanel(private val project: Project) : SimpleToolWindowPanel(true) {
+class VitestToolWindowPanel(private val project: Project) : SimpleToolWindowPanel(true) {
     private val usagePanel = UsagePanel("Vitest plugin usage:\n")
+    private val vitestTreeModel = DefaultMutableTreeNode("Vitest")
+    private var vitestTree: Tree? = null
     private val jbangToolWindow = vitestTree()
     var mode: String = "help"
 
@@ -40,8 +49,20 @@ class JBangToolWindowPanel(private val project: Project) : SimpleToolWindowPanel
         }
     }
 
-    fun refreshScriptInfo(project: Project) {
-
+    fun refreshVitestTree(project: Project, vitestRestResult: VitestTestResult) {
+        val projectDir = project.guessProjectDir()?.canonicalPath ?: ""
+        ApplicationManager.getApplication().invokeLater {
+            vitestTreeModel.removeAllChildren()
+            vitestRestResult.testResults?.forEach { testResult ->
+                val testFileNode = DefaultMutableTreeNode(testFileName(projectDir, testResult.name!!))
+                testResult.assertionResults?.forEach { assertionResult ->
+                    testFileNode.add(DefaultMutableTreeNode(assertionResult))
+                }
+                vitestTreeModel.add(testFileNode)
+            }
+            (vitestTree!!.model as DefaultTreeModel).reload()
+            expandVitestTree()
+        }
     }
 
     fun switchToHelp() {
@@ -56,7 +77,6 @@ class JBangToolWindowPanel(private val project: Project) : SimpleToolWindowPanel
 
     private fun vitestTree(): JComponent {
         val vitestRestResult = project.getService(VitestService::class.java).vitestRestResult
-        val vitestTreeModel = DefaultMutableTreeNode("Vitest")
         val projectDir = project.guessProjectDir()?.canonicalPath ?: ""
         if (vitestRestResult != null) {
             vitestRestResult.testResults?.forEach { testResult ->
@@ -67,9 +87,38 @@ class JBangToolWindowPanel(private val project: Project) : SimpleToolWindowPanel
                 vitestTreeModel.add(testFileNode)
             }
         }
-        val tree = Tree(vitestTreeModel)
-        tree.cellRenderer = VitestTreeCellRender()
-        return JBScrollPane(tree)
+        vitestTree = Tree(DefaultTreeModel(vitestTreeModel))
+        vitestTree!!.cellRenderer = VitestTreeCellRender()
+        expandVitestTree()
+        vitestTree!!.addMouseListener(object : MouseAdapter() {
+            override fun mouseClicked(e: MouseEvent) {
+                if (e.clickCount == 2) {
+                    val node = vitestTree!!.lastSelectedPathComponent as DefaultMutableTreeNode
+                    if (node.isLeaf) {
+                        val assertionResult = node.userObject as AssertionResult
+                        val testName = assertionResult.title
+                        val filePath = (node.parent as DefaultMutableTreeNode).userObject.toString()
+                        project.guessProjectDir()?.let {
+                            it.findFileByRelativePath(filePath)?.let { testedFile ->
+                                val psiFile = PsiManager.getInstance(project).findFile(testedFile)
+                                val lineNum = psiFile!!.text.lines().indexOfFirst { line ->
+                                    line.contains("'${testName}'") || line.contains("\"${testName}\"")
+                                }
+                                val fileEditorManager = FileEditorManager.getInstance(project)
+                                fileEditorManager.openTextEditor(OpenFileDescriptor(project, testedFile, lineNum, 0), true)
+                            }
+                        }
+                    }
+                }
+            }
+        })
+        return JBScrollPane(vitestTree)
+    }
+
+    private fun expandVitestTree() {
+        for (i in 0 until vitestTree!!.rowCount) {
+            vitestTree!!.expandRow(i)
+        }
     }
 
     private fun testFileName(projectDir: String, testFilePath: String): String {
