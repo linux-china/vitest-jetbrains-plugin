0c8c9c3aeb3320ecfad2713fc20360c006ead48c
linux_china <libing.chen@gmail.com>
2022-05-22 11:09:28 -0700

    docs: adjust description

--- src/main/kotlin/com/github/linuxchina/jetbrains/plugins/vitest/ui/UsagePanel.kt
+++ src/main/kotlin/com/github/linuxchina/jetbrains/plugins/vitest/ui/UsagePanel.kt
@@ -5,12 +5,20 @@ import javax.swing.JTextArea
 class UsagePanel constructor(prefix: String) : JTextArea() {
     private val vitestPluginUsage = """
 Setup: 
-Add following command into `"webstorm-integration": "vitest --watch --reporter=verbose --reporter=json --outputFile=.vitest-result.json",` package.json scripts
+
+- Add following command into package.json scripts
+
+```
+"scripts": {
+ "webstorm-integration": "vitest --watch --reporter=verbose --reporter=json --outputFile=.vitest-result.json",
+}
+```
 
 Features:
 - Green Run icon means to run once only
 - Vitest Run icon means to run with watch mode
-- Failure detection to run test method
+- Test failure detection to gutter
+- Test failure detection to gutter
 - Vitest json reporter integration with Vitest run icons by `.vitest-result.json` file
 
 """
