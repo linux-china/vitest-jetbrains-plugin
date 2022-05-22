af6c4cd083615110b119cb90c2857bdb8b22f90e
linux_china <libing.chen@gmail.com>
2022-05-22 00:22:48 -0700

    feat: add Vitest toolWindow refresh

--- src/main/kotlin/com/github/linuxchina/jetbrains/plugins/vitest/VitestService.kt
+++ src/main/kotlin/com/github/linuxchina/jetbrains/plugins/vitest/VitestService.kt
@@ -2,6 +2,7 @@ package com.github.linuxchina.jetbrains.plugins.vitest
 
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.module.kotlin.readValue
+import com.github.linuxchina.jetbrains.plugins.vitest.ui.VitestToolWindowPanel
 import com.intellij.codeInsight.daemon.DaemonCodeAnalyzer
 import com.intellij.openapi.application.ApplicationManager
 import com.intellij.openapi.project.Project
@@ -10,6 +11,7 @@ import com.intellij.openapi.roots.ProjectFileIndex
 import com.intellij.openapi.vfs.VirtualFileManager
 import com.intellij.openapi.vfs.newvfs.BulkFileListener
 import com.intellij.openapi.vfs.newvfs.events.VFileEvent
+import com.intellij.openapi.wm.ToolWindowManager
 import com.intellij.psi.PsiManager
 
 
@@ -21,21 +23,20 @@ class VitestService(private val project: Project) {
     init {
         checkViteConfig()
         reloadVitestJsonResultFile()
-        project.messageBus.connect().subscribe(VirtualFileManager.VFS_CHANGES,
-            object : BulkFileListener {
-                override fun after(events: List<VFileEvent>) {
-                    events.forEach { vFileEvent ->
-                        val changedFile = vFileEvent.file
-                        if (changedFile != null) {
-                            if (changedFile.name == ".vitest-result.json") {
-                                if (ProjectFileIndex.getInstance(project).isInContent(changedFile)) {
-                                    reloadVitestJsonResultFile()
-                                }
+        project.messageBus.connect().subscribe(VirtualFileManager.VFS_CHANGES, object : BulkFileListener {
+            override fun after(events: List<VFileEvent>) {
+                events.forEach { vFileEvent ->
+                    val changedFile = vFileEvent.file
+                    if (changedFile != null) {
+                        if (changedFile.name == ".vitest-result.json") {
+                            if (ProjectFileIndex.getInstance(project).isInContent(changedFile)) {
+                                reloadVitestJsonResultFile()
                             }
                         }
                     }
                 }
-            })
+            }
+        })
     }
 
     private fun checkViteConfig() {
@@ -56,6 +57,7 @@ class VitestService(private val project: Project) {
                 val refreshRequired = vitestRestResult != null
                 vitestRestResult = objectMapper.readValue<VitestTestResult>(it.toNioPath().toFile())
                 if (refreshRequired) {
+                    getVitestToolWindowPanel(project)?.refreshVitestTree(project, vitestRestResult!!)
                     ApplicationManager.getApplication().runReadAction {
                         DaemonCodeAnalyzer.getInstance(project).restart()
                     }
@@ -66,5 +68,14 @@ class VitestService(private val project: Project) {
 
     fun findTestResult(filePath: String, testName: String): AssertionResult? {
         return vitestRestResult?.findTestResult(filePath, testName)
+    }
+
+    private fun getVitestToolWindowPanel(project: Project): VitestToolWindowPanel? {
+        val toolWindow = ToolWindowManager.getInstance(project).getToolWindow("Vitest")
+        if (toolWindow != null) {
+            val content = toolWindow.contentManager.contents.first { it.component is VitestToolWindowPanel }
+            return content.component as VitestToolWindowPanel
+        }
+        return null
     }
 }
