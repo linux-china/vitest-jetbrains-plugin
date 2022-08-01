1c35def86c33704a17d60d37c68d09ab2da08663
linux_china <libing.chen@gmail.com>
2022-08-01 13:16:25 -0700

    feat: implement debug logic

--- src/main/kotlin/com/github/linuxchina/jetbrains/plugins/vitest/VitestRunLineMarkerContributor.kt
+++ src/main/kotlin/com/github/linuxchina/jetbrains/plugins/vitest/VitestRunLineMarkerContributor.kt
@@ -1,10 +1,17 @@
 package com.github.linuxchina.jetbrains.plugins.vitest
 
+import com.intellij.execution.Location
+import com.intellij.execution.PsiLocation
+import com.intellij.execution.RunManagerEx
+import com.intellij.execution.actions.ConfigurationContext
+import com.intellij.execution.executors.DefaultDebugExecutor
 import com.intellij.execution.lineMarker.RunLineMarkerContributor
+import com.intellij.execution.runners.ExecutionEnvironmentBuilder
 import com.intellij.lang.javascript.psi.JSCallExpression
 import com.intellij.lang.javascript.psi.JSReferenceExpression
 import com.intellij.openapi.actionSystem.AnAction
 import com.intellij.openapi.actionSystem.AnActionEvent
+import com.intellij.openapi.actionSystem.impl.SimpleDataContext
 import com.intellij.psi.PsiElement
 
 class VitestRunLineMarkerContributor : RunLineMarkerContributor() {
@@ -20,9 +27,15 @@ class VitestRunLineMarkerContributor : RunLineMarkerContributor() {
                 }
                 val debugAction = object : AnAction("Debugger", "Debug Vitest test", debuggerIcon) {
                     override fun actionPerformed(e: AnActionEvent) {
-                        println("action1")
+                        val dataContext = SimpleDataContext.getSimpleContext(Location.DATA_KEY, PsiLocation(element), e.dataContext)
+                        val context = ConfigurationContext.getFromContext(dataContext, e.place)
+                        val producer = VitestTestRunConfigurationProducer()
+                        val configurationSettings = producer.findOrCreateConfigurationFromContext(context)?.configurationSettings ?: return
+                        (context.runManager as RunManagerEx).setTemporaryConfiguration(configurationSettings)
+                        // start debug
+                        ExecutionEnvironmentBuilder.create(e.project!!, DefaultDebugExecutor.getDebugExecutorInstance(), configurationSettings.configuration).buildAndExecute()
+                        //ExecutionUtil.runConfiguration(configuration, Executor.EXECUTOR_EXTENSION_NAME.extensionList.first())
                     }
-
                 }
                 val actions: Array<AnAction> = arrayOf(watchAction, debugAction)
                 return Info(vitestIcon, actions) {
